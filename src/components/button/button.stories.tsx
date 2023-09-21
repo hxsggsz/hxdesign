dfdd691db0e447d6a98e746d8e6da1337208ff98
hxsggsz <vt.hugo.2021@gmail.com>
2023-09-20 22:28:33 -0300

    feature: add button component with all a11y rules

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -1,15 +1,40 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Button } from './button'
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./button";
 
 const meta = {
-  title: 'button',
+  title: "button",
   component: Button,
-} satisfies Meta<typeof Button>
+} satisfies Meta<typeof Button>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-  args: {},
+  args: {
+    children: "minecraft",
+    variant: false,
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    children: "minecraft",
+    variant: true,
+  },
+  parameters: {
+    a11y: {
+      element: "#storybook-root",
+      config: {
+        rules: [
+          {
+            id: "color-contrast",
+            enabled: false,
+          },
+        ],
+      },
+      options: {},
+      manual: true,
+    },
+  },
 };
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,7 +1,34 @@
-export const Button = () => {
+import { ButtonProps } from "./button.types";
+import scss from "./button.module.scss";
+import { useState } from "react";
+import minecraftClickSound from "./assets/minecraft_click.mp3";
+import classNames from "classnames";
+
+export const Button = (props: ButtonProps) => {
+  const clickSound = new Audio(minecraftClickSound);
+
+  const [isClicked, setIsClicked] = useState(false);
+
+  const inputClasses = classNames({
+    [scss.grass]: props.variant,
+    [scss.defaultBtn]: !props.variant,
+  });
+
+  function handleClickCapture() {
+    clickSound.play();
+    setIsClicked(true);
+    setTimeout(() => setIsClicked(false), 1000);
+  }
+
   return (
-    <button>
-      teste
+    <button
+      {...props}
+      className={inputClasses}
+      aria-pressed={isClicked}
+      aria-disabled={props.disabled}
+      onClickCapture={handleClickCapture}
+    >
+      {props.children}
     </button>
-  )
-}
+  );
+};
