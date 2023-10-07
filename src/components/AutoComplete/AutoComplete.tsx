49df87387c618d1ba722a90d173a734e9875be3a
hxsggsz <vt.hugo.2021@gmail.com>
2023-10-07 20:08:16 -0300

    a11y: add aria attributes following WAI-ARIA design pattern

--- src/components/AutoComplete/AutoComplete.tsx
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -6,11 +6,13 @@ import { Close } from "../Icons/Close/Close";
 import { Button } from "../Button/Button";
 import { AutoCompleteProps, ListProps } from "./AutoComplete.types";
 import useClickOutside from "../../hooks/useClickOutside";
+import { useRovingTabIndex } from "@20i/use-roving-tabindex-a11y";
 
 export const AutoComplete = (props: AutoCompleteProps) => {
   const [search, setSearch] = useState("");
   const [isOpen, setIsOpen] = useState(false);
 
+  const listRef = useRovingTabIndex();
   const ref = useClickOutside(() => handleOpen(false));
 
   function handleChange(ev: React.ChangeEvent<HTMLInputElement>) {
@@ -26,25 +28,17 @@ export const AutoComplete = (props: AutoCompleteProps) => {
   }
 
   function handleKeyDown(ev: React.KeyboardEvent<HTMLDivElement>) {
-    if (ev.key === "Escape") handleOpen();
-  }
-
-  function handleNavigateList(
-    ev: React.KeyboardEvent<HTMLLIElement>,
-    index: number
-  ) {
     switch (ev.key) {
-      case "ArrowDown":
-        ev.preventDefault();
-        console.log("aaa");
-        return document.querySelectorAll("button")[index - 1]?.focus();
+      case "Escape":
+        if (!isOpen) {
+          return clearInput();
+        }
+
+        handleOpen();
         break;
 
-      case "ArrowUp":
-        ev.preventDefault();
-        return document.querySelectorAll("button")[index + 1]?.focus();
-        break;
-      default:
+      case "Enter":
+        if (isOpen) handleOpen();
         break;
     }
   }
@@ -63,31 +57,41 @@ export const AutoComplete = (props: AutoCompleteProps) => {
     </li>
   );
 
+  const renderListItem = () =>
+    filterList.map((list, index) => (
+      <li
+        key={list.id}
+        role="option"
+        data-open={isOpen}
+        className={scss.item}
+        aria-selected={search !== ""}
+      >
+        <button
+          tabIndex={index === 0 ? 0 : -1}
+          aria-expanded={isOpen}
+          className={scss.button}
+          onClick={() => {
+            handleOpen();
+            clearInput();
+            setSearch(list.item);
+            list.onSelect(list.item);
+          }}
+        >
+          {list.item}
+        </button>
+      </li>
+    ));
+
   const renderList = () => (
-    <ul data-open={isOpen} aria-hidden={!isOpen} className={scss.list}>
-      {filterList.length > 0
-        ? filterList.map((list, index) => (
-            <li
-              // tabIndex={-1}
-              key={list.id}
-              data-open={isOpen}
-              className={scss.item}
-              onKeyDown={(ev) => handleNavigateList(ev, index)}
-            >
-              <button
-                className={scss.button}
-                onClick={() => {
-                  handleOpen();
-                  clearInput();
-                  setSearch(list.item);
-                  list.onSelect(list.item);
-                }}
-              >
-                {list.item}
-              </button>
-            </li>
-          ))
-        : renderNotFoundList()}
+    <ul
+      ref={listRef}
+      role="listbox"
+      id="cb1-listbox"
+      data-open={isOpen}
+      aria-hidden={!isOpen}
+      className={scss.list}
+    >
+      {filterList.length > 0 ? renderListItem() : renderNotFoundList()}
     </ul>
   );
 
@@ -98,17 +102,32 @@ export const AutoComplete = (props: AutoCompleteProps) => {
           <Arrow variant outline orientation={`${isOpen ? "down" : "up"}`} />
         </Input.Actions>
         <Input.Input
+          id="cb1-input"
           fontSize={2.4}
-          spellCheck="false"
-          onChange={handleChange}
           value={search}
+          role="combobox"
+          spellCheck="false"
+          aria-expanded={isOpen}
+          aria-autocomplete="list"
+          onChange={handleChange}
+          aria-controls="cb1-listbox"
+          aria-label="input for search"
           onClick={() => handleOpen()}
           placeholder={props.placeholder}
         />
         <Input.Actions>
-          <Button variant="none" onClick={clearInput}>
-            <Close variant />
-          </Button>
+          {isOpen && (
+            <Button
+              id="cb1-button"
+              variant="none"
+              onClick={clearInput}
+              aria-expanded={isOpen}
+              aria-controls="cb1-listbox"
+              aria-label="indicator of autocomplete"
+            >
+              <Close variant />
+            </Button>
+          )}
         </Input.Actions>
       </Input.Root>
       {isOpen && renderList()}
